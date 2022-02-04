841748b2bcdced661cc82f22a703ecbd5878ff09
Aakash Sharma <96709665+Aacashh@users.noreply.github.com>
2022-02-04 16:12:49 +0530

    Add files via upload
    Answer 6 & 7

---
+++ Aeronav/Assignment_1/Answer6.py
@@ -0,0 +1,14 @@
+def iterations(word):
+    if len(word) == 1:
+        return [word]
+
+    perms = iterations(word[1:])
+    char = word[0]
+    result = []
+    for perm in perms:
+        for i in range(len(perm)+1):
+            result.append(perm[:i]+ char + perm[i:])
+    
+    return result              
+s = 'ABC'
+print(iterations(s)+ iterations(s.lower()))
